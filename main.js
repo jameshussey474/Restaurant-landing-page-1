e0af75a56c4cd274c2995a4b6ee899fac9e1d389
paul-mcj <83841390+paul-mcj@users.noreply.github.com>
2021-11-07 12:59:53 -0500

    Add files via upload
    CSS
    - increased the font size for mobile, as well as made the font bold (it was too difficult to read on small screens)
    - padding bottom for the main li items of the drop down menu is increased
    - font for custom property --black-1 was changed to a slightly lighter black for contrast purposes
    - the entire grid for main__menu-section has been redone for mobile, keeping the images in a new separate grid from the menu-article
    
    
    HTML
    - all social media links now go to a fake page that represents Mehr's account
    - the "read more" button on private-dining-section now goes to a new html page for private dining
    - added a new grid-menu for the images of mein__menu-section
    
    
    JS
    - fixed an issue with opening the sub-menus for the "menus" and "contact" li elements of the nav bar. Instead of creating one function that toggles the sub-menu-open class for both, each has its own to keep the toggling of the classes separate. Was causing issues on Chrome/Brave browsers.
    - fixed an issue with the iFrame element messing with the page layout. Horizontal padding for its container is calculated and then subtracted from the iFrame's innerWidth to center it without causing left or right overflowing, which became apparent with Chrome on Android.

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ let time = 9000; // declare how long slides will last in milliseconds for the au
 
 // Event Listeners
 hamburger.addEventListener("click", openMenu);
-subMenuMenus.addEventListener("click", openSubMenu);
-subMenuContact.addEventListener("click", openSubMenu);
+subMenuMenus.addEventListener("click", openSubMenuMenus);
+subMenuContact.addEventListener("click", openSubMenuContact);
 
 // Automatic slideshow
 autoSlideshow();
@@ -26,9 +26,13 @@ function openMenu(event) {
 
 // Toggles the "sub-menu-open" for some navbar elements to expose more links below. Also tries to emulate transformations on mobile displays, as hovers aren't prevalent.
 // let after = document.styleSheets[0].cssRules[22];
-function openSubMenu(event) {
-    let extended = event.target.parentElement.children[1];
-    extended.classList.toggle("sub-menu-open");
+function openSubMenuMenus(event) {
+    event.target.parentElement.children[1].classList.toggle("sub-menu-open");
+    // extended.classList.toggle("sub-menu-open");
+}
+
+function openSubMenuContact(event) {
+    event.target.parentElement.children[1].classList.toggle("sub-menu-open");
 }
 
 // Automatically cycles through images within the inner-slider-images html section
@@ -40,12 +44,17 @@ function autoSlideshow() {
     current < images.length - 1 ? current++ : (current = 0);
     images[current].style.display = "block";
     innerSliderImages.style.animation = "rollDown";
-    console.log(innerSliderImages.style.animation);
+    // console.log(innerSliderImages.style.animation);
     setTimeout(autoSlideshow, time);
 }
 
-// The map needs to have a dynamic sizing depending on the size of screen the user has
+// fixme: domcontentloaded??
+// The map needs to have a dynamic sizing depending on the size of screen the user has.
 const iFrame = document.querySelector("iframe");
-let iFrameWidth = window.innerWidth;
+let iFrameWidth = window.innerWidth; //grab the value for this elements horizontal padding (ex. 2em) and multiply it by the computed font-size . so 16 * 2 = 32. Since the padding is left and right, multiply by 2 again = 64!
+const bottomSectionMap = document.querySelector(".bottom-section__map");
+let mapPadding = window.getComputedStyle(bottomSectionMap).getPropertyValue(`padding-left`); //get the padding for the bottomSectionMap
+iFrameWidth -= parseInt(mapPadding) * 2;
+console.log(iFrameWidth);
 iFrame.setAttribute("height", iFrameWidth);
 iFrame.setAttribute("width", iFrameWidth);
